ce2576092f15838f4e4034932ffcd615c351c6d7
JAICHANGPARK <aristojeff@gmail.com>
2024-12-20 08:52:40 +0900

    Update food_recipes_main_screen.dart

--- lib/ep2101_food_recipes_app/views/food_recipes_main_screen.dart
+++ lib/ep2101_food_recipes_app/views/food_recipes_main_screen.dart
@@ -1,2 +1,16 @@
 import 'package:flutter/material.dart';
 
+
+class FoodRecipesMainScreen extends StatefulWidget {
+  const FoodRecipesMainScreen({super.key});
+
+  @override
+  State<FoodRecipesMainScreen> createState() => _FoodRecipesMainScreenState();
+}
+
+class _FoodRecipesMainScreenState extends State<FoodRecipesMainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
