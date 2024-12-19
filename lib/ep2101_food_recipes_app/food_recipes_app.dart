dfa1fd98ad6db0b073f72fbe2d4d5c603bdaf55a
JAICHANGPARK <aristojeff@gmail.com>
2024-12-20 08:49:37 +0900

    Create food_recipes_app.dart

---
+++ lib/ep2101_food_recipes_app/food_recipes_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class FoodRecipesApp extends StatelessWidget {
+  const FoodRecipesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
