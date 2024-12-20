5b827f36afaf4a142879503831a060da5eb0414e
JAICHANGPARK <aristojeff@gmail.com>
2024-12-21 08:42:32 +0900

    Update food_recipes_home_page.dart

--- lib/ep2101_food_recipes_app/views/widgets/food_recipes_home_page.dart
+++ lib/ep2101_food_recipes_app/views/widgets/food_recipes_home_page.dart
@@ -2,12 +2,17 @@ import 'package:flutter/material.dart';
 
 
 class FoodRecipesHomePage extends StatelessWidget {
-  const FoodRecipesHomePage({super.key});
+  FoodRecipesHomePage({super.key});
 
   TextEditingController textEditingController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+
+      ],
+    );
   }
 }
