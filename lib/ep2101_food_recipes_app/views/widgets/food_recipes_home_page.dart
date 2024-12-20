5deda72050ee4a167b69627c417f97ae9bfb33c8
JAICHANGPARK <aristojeff@gmail.com>
2024-12-21 08:42:14 +0900

    Create food_recipes_home_page.dart

---
+++ lib/ep2101_food_recipes_app/views/widgets/food_recipes_home_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class FoodRecipesHomePage extends StatelessWidget {
+  const FoodRecipesHomePage({super.key});
+
+  TextEditingController textEditingController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
