13aaf81e32373ef9350ac98bf85ef6c017f7fd4a
JAICHANGPARK <aristojeff@gmail.com>
2024-12-21 08:49:32 +0900

    Update food_recipes_home_page.dart

--- lib/ep2101_food_recipes_app/views/widgets/food_recipes_home_page.dart
+++ lib/ep2101_food_recipes_app/views/widgets/food_recipes_home_page.dart
@@ -11,39 +11,48 @@ class FoodRecipesHomePage extends StatelessWidget {
       spacing: 16,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Row(
-          spacing: 8,
-          children: [
-            Icon(Icons.location_on_outlined),
-            Text(
-              "Now Los Angeles",
-            ),
-            Icon(Icons.keyboard_arrow_down),
-            Spacer(),
-            Icon(Icons.notifications_none),
-          ],
-        ),
-        Container(
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.grey),
-            borderRadius: BorderRadius.circular(24),
-          ),
-          padding: EdgeInsets.symmetric(horizontal: 16),
-          child: TextField(
-            controller: textEditingController,
-
-            decoration: InputDecoration(
-              border: InputBorder.none,
-              icon: Icon(Icons.search),
-              suffixIcon: Icon(Icons.mic_none),
-              hintText: "What do you want today?",
-              contentPadding: EdgeInsets.only(
-                top: 12
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Row(
+                spacing: 8,
+                children: [
+                  Icon(Icons.location_on_outlined),
+                  Text(
+                    "Now Los Angeles",
+                  ),
+                  Icon(Icons.keyboard_arrow_down),
+                  Spacer(),
+                  Icon(Icons.notifications_none),
+                ],
               ),
-            ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(24),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 16),
 
+                child: TextField(
+                  controller: textEditingController,
+
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    icon: Icon(Icons.search),
+                    suffixIcon: Icon(Icons.mic_none),
+                    hintText: "What do you want today?",
+                    contentPadding: EdgeInsets.only(
+                        top: 12
+                    ),
+                  ),
+
+                ),
+              )
+            ],
           ),
         )
+
       ],
     );
   }
