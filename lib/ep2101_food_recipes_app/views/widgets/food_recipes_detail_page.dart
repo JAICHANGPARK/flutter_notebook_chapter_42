526ae2bf6811a27dcce0457db1353e787ab54fde
JAICHANGPARK <aristojeff@gmail.com>
2024-12-23 18:25:02 +0900

    Update food_recipes_detail_page.dart

--- lib/ep2101_food_recipes_app/views/widgets/food_recipes_detail_page.dart
+++ lib/ep2101_food_recipes_app/views/widgets/food_recipes_detail_page.dart
@@ -12,18 +12,35 @@ class _FoodRecipesDetailPageState extends State<FoodRecipesDetailPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          spacing: 12,
-          children: [
-            Container(
-              height: 280,
-              decoration: BoxDecoration(
-                color: Colors.grey,
-                borderRadius: BorderRadius.circular(12)
-              ),
-
-            )
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            spacing: 12,
+            children: [
+              Container(
+                height: 280,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(12),
+                  image: DecorationImage(
+                    image: NetworkImage(
+                      "https://cdn.pixabay.com/photo/2020/10/05/19/55/hamburger-5630646_1280.jpg",
+                    ),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                padding: EdgeInsets.all(16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: Colors.white,
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
