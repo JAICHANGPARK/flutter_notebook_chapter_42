970aa0825d5c69c8509f6f448dffe786e9cbb725
JAICHANGPARK <aristojeff@gmail.com>
2024-12-23 18:19:32 +0900

    Update food_recipes_detail_page.dart

--- lib/ep2101_food_recipes_app/views/widgets/food_recipes_detail_page.dart
+++ lib/ep2101_food_recipes_app/views/widgets/food_recipes_detail_page.dart
@@ -1,3 +1,18 @@
 
 import 'package:flutter/material.dart';
 
+
+class FoodRecipesDetailPage extends StatefulWidget {
+  const FoodRecipesDetailPage({super.key});
+
+  @override
+  State<FoodRecipesDetailPage> createState() => _FoodRecipesDetailPageState();
+}
+
+class _FoodRecipesDetailPageState extends State<FoodRecipesDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
