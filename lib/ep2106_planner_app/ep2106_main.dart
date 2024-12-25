6f7712c8c13f3ff97d16a196a7cbac4ce820dea8
JAICHANGPARK <aristojeff@gmail.com>
2024-12-26 08:25:42 +0900

    Update ep2106_main.dart

--- lib/ep2106_planner_app/ep2106_main.dart
+++ lib/ep2106_planner_app/ep2106_main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_42/ep2106_planner_app/daily_planner_app.dart';
 
-
 void main() {
   runApp(DailyPlannerApp());
-
 }
-
