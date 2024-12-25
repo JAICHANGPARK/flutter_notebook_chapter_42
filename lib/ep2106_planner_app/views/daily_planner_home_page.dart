3d036eb5fab81cc6b659d212e613338d562f6143
JAICHANGPARK <aristojeff@gmail.com>
2024-12-25 09:50:47 +0900

    Update daily_planner_home_page.dart

--- lib/ep2106_planner_app/views/daily_planner_home_page.dart
+++ lib/ep2106_planner_app/views/daily_planner_home_page.dart
@@ -24,8 +24,8 @@ class _DailyPlannerHomePageState extends State<DailyPlannerHomePage> {
                   ),
                   shape: BoxShape.circle,
                 ),
-                p
-                child: Icon(Icons.menu, size: 16,),
+                padding: EdgeInsets.all(4),
+                child: Icon(Icons.menu, size: 18,),
               )
             ],
           )
