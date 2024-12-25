118449a51ea45913aed72fb78f6dc022184ee5dd
JAICHANGPARK <aristojeff@gmail.com>
2024-12-25 09:47:16 +0900

    Update daily_planner_home_page.dart

--- lib/ep2106_planner_app/views/daily_planner_home_page.dart
+++ lib/ep2106_planner_app/views/daily_planner_home_page.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class DailyPlannerHomePage extends StatefulWidget {
+  const DailyPlannerHomePage({super.key});
+
+  @override
+  State<DailyPlannerHomePage> createState() => _DailyPlannerHomePageState();
+}
+
+class _DailyPlannerHomePageState extends State<DailyPlannerHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
