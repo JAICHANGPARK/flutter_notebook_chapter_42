e27375f867afaa29c5b99df9abb7cd3fb2d56731
JAICHANGPARK <aristojeff@gmail.com>
2024-12-28 09:39:09 +0900

    Update hotel_booking_app.dart

--- lib/ep2109_hotel_book_app/hotel_booking_app.dart
+++ lib/ep2109_hotel_book_app/hotel_booking_app.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 
+import 'views/hotel_book_home_page.dart';
+
 class HotelBookingApp extends StatelessWidget {
   const HotelBookingApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: HotelBookHomePage(),
+    );
   }
 }
 
