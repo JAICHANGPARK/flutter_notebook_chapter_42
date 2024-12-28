e3d4b454614879010d4db3b5c709ac069cfa15c5
JAICHANGPARK <aristojeff@gmail.com>
2024-12-28 09:38:59 +0900

    Update hotel_book_home_page.dart

--- lib/ep2109_hotel_book_app/views/hotel_book_home_page.dart
+++ lib/ep2109_hotel_book_app/views/hotel_book_home_page.dart
@@ -1,3 +1,17 @@
 
 import 'package:flutter/material.dart';
 
+
+class HotelBookHomePage extends StatefulWidget {
+  const HotelBookHomePage({super.key});
+
+  @override
+  State<HotelBookHomePage> createState() => _HotelBookHomePageState();
+}
+
+class _HotelBookHomePageState extends State<HotelBookHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
