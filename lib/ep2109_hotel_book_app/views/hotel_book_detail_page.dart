74400e3abf13d96832c10e8cefd045ee909a4031
Dreamwalker <aristojeff@gmail.com>
2024-12-30 09:34:43 +0900

    Update hotel_book_detail_page.dart

--- lib/ep2109_hotel_book_app/views/hotel_book_detail_page.dart
+++ lib/ep2109_hotel_book_app/views/hotel_book_detail_page.dart
@@ -1,2 +1,15 @@
 import 'package:flutter/material.dart';
 
+class HotelBookDetailPage extends StatefulWidget {
+  const HotelBookDetailPage({super.key});
+
+  @override
+  State<HotelBookDetailPage> createState() => _HotelBookDetailPageState();
+}
+
+class _HotelBookDetailPageState extends State<HotelBookDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
