d7fb3ad89b6a7e1d0ac3ee29fb09735b6d2dcddf
Dreamwalker <aristojeff@gmail.com>
2024-12-30 09:37:26 +0900

    Update hotel_book_detail_page.dart

--- lib/ep2109_hotel_book_app/views/hotel_book_detail_page.dart
+++ lib/ep2109_hotel_book_app/views/hotel_book_detail_page.dart
@@ -10,6 +10,25 @@ class HotelBookDetailPage extends StatefulWidget {
 class _HotelBookDetailPageState extends State<HotelBookDetailPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Column(
+              children: [],
+            ),
+          ),
+          Positioned(
+              bottom: 24,
+              left: 24,
+              right: 24,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(15, 86, 70, 1),
+                ),
+              ))
+        ],
+      ),
+    );
   }
 }
