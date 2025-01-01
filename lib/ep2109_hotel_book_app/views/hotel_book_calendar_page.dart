811ae3aa9c0dce0b90f713100179acd4e634cae8
JAICHANGPARK <aristojeff@gmail.com>
2025-01-01 09:44:30 +0900

    Update hotel_book_calendar_page.dart

---
+++ lib/ep2109_hotel_book_app/views/hotel_book_calendar_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HotelBookCalendarPage extends StatefulWidget {
+  const HotelBookCalendarPage({super.key});
+
+  @override
+  State<HotelBookCalendarPage> createState() => _HotelBookCalendarPageState();
+}
+
+class _HotelBookCalendarPageState extends State<HotelBookCalendarPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
