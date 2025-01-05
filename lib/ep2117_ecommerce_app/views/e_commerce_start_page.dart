2bd4da9add5b920b53834df14db8e0a9ec6c6672
JAICHANGPARK <aristojeff@gmail.com>
2025-01-05 10:00:17 +0900

    Update e_commerce_start_page.dart

--- lib/ep2117_ecommerce_app/views/e_commerce_start_page.dart
+++ lib/ep2117_ecommerce_app/views/e_commerce_start_page.dart
@@ -1,2 +1,10 @@
 import 'package:flutter/material.dart';
 
+class ECommerceStartPage extends StatelessWidget {
+  const ECommerceStartPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
