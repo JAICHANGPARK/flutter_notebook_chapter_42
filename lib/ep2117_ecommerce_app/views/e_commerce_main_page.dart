c5d7621c70837d1fdd4564b5e2a31179c5f8e795
JAICHANGPARK <aristojeff@gmail.com>
2025-01-05 09:52:31 +0900

    Create e_commerce_main_page.dart

---
+++ lib/ep2117_ecommerce_app/views/e_commerce_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class ECommerceMainPage extends StatefulWidget {
+  const ECommerceMainPage({super.key});
+
+  @override
+  State<ECommerceMainPage> createState() => _ECommerceMainPageState();
+}
+
+class _ECommerceMainPageState extends State<ECommerceMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
