4c12aa3ae68a431841a75730692a613b68cbb115
JAICHANGPARK <aristojeff@gmail.com>
2025-01-09 18:43:07 +0900

    Update e_commerce_main_page.dart

--- lib/ep2117_ecommerce_app/views/e_commerce_main_page.dart
+++ lib/ep2117_ecommerce_app/views/e_commerce_main_page.dart
@@ -435,56 +435,59 @@ class _ECommerceMainPageState extends State<ECommerceMainPage> {
                       ],
                     ),
                   )),
-                  Container(
-                      height: 100,
-                      margin: EdgeInsets.symmetric(horizontal: 16),
-                      child: Center(
-                        child: Container(
-                          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(32),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.black.withValues(alpha: .1),
-                                blurRadius: 16,
-                                spreadRadius: 8,
-                              )
-                            ],
-                          ),
-                          child: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            spacing: 12,
-                            children: [
-                              CircleAvatar(
-                                backgroundColor: Colors.black,
-                                foregroundColor: Colors.white,
-                                child: Icon(Icons.home_filled),
-                              ),
-                              CircleAvatar(
-                                backgroundColor: Colors.white,
-                                foregroundColor: Colors.black,
-                                child: Icon(Icons.store),
-                              ),
-                              CircleAvatar(
-                                backgroundColor: Colors.white,
-                                foregroundColor: Colors.black,
-                                child: Icon(Icons.favorite_border),
-                              ),
-                              CircleAvatar(
-                                backgroundColor: Colors.white,
-                                foregroundColor: Colors.black,
-                                child: Icon(Icons.person_2_outlined),
-                              )
-                            ],
-                          ),
-                        ),
-                      ))
+
                 ],
               ),
             ),
           ),
+          Positioned(
+              
+              child:    Container(
+              height: 100,
+              margin: EdgeInsets.symmetric(horizontal: 16),
+              child: Center(
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(32),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withValues(alpha: .1),
+                        blurRadius: 16,
+                        spreadRadius: 8,
+                      )
+                    ],
+                  ),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    spacing: 12,
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: Colors.black,
+                        foregroundColor: Colors.white,
+                        child: Icon(Icons.home_filled),
+                      ),
+                      CircleAvatar(
+                        backgroundColor: Colors.white,
+                        foregroundColor: Colors.black,
+                        child: Icon(Icons.store),
+                      ),
+                      CircleAvatar(
+                        backgroundColor: Colors.white,
+                        foregroundColor: Colors.black,
+                        child: Icon(Icons.favorite_border),
+                      ),
+                      CircleAvatar(
+                        backgroundColor: Colors.white,
+                        foregroundColor: Colors.black,
+                        child: Icon(Icons.person_2_outlined),
+                      )
+                    ],
+                  ),
+                ),
+              )))
         ],
       ),
     );
