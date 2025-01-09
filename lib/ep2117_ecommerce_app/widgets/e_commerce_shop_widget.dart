de6bfe9749ed0ae9025d597a1a5b8d499a8f57b3
JAICHANGPARK <aristojeff@gmail.com>
2025-01-09 18:49:03 +0900

    Update e_commerce_shop_widget.dart

--- lib/ep2117_ecommerce_app/widgets/e_commerce_shop_widget.dart
+++ lib/ep2117_ecommerce_app/widgets/e_commerce_shop_widget.dart
@@ -11,45 +11,52 @@ class ECommerceShopWidget extends StatefulWidget {
 class _ECommerceShopWidgetState extends State<ECommerceShopWidget> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Container(
-          height: 52,
-          margin: EdgeInsets.symmetric(horizontal: 16),
-          padding: EdgeInsets.only(left: 8, right: 4, top: 2, bottom: 2),
-          decoration: BoxDecoration(
-            border: Border.all(
-              color: Colors.grey[200]!,
-            ),
-            borderRadius: BorderRadius.circular(32),
-          ),
-          child: Row(
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
             children: [
-              Expanded(
-                  child: TextField(
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      hintText: "Search",
-                      icon: Icon(Icons.search),
-                    ),
-                  )),
               Container(
+                height: 52,
+                margin: EdgeInsets.symmetric(horizontal: 16),
+                padding: EdgeInsets.only(left: 8, right: 4, top: 2, bottom: 2),
                 decoration: BoxDecoration(
                   border: Border.all(
                     color: Colors.grey[200]!,
                   ),
-                  shape: BoxShape.circle,
+                  borderRadius: BorderRadius.circular(32),
                 ),
-                child: IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.tune),
+                child: Row(
+                  children: [
+                    Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Search",
+                            icon: Icon(Icons.search),
+                          ),
+                        )),
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.grey[200]!,
+                        ),
+                        shape: BoxShape.circle,
+                      ),
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.tune),
+                      ),
+                    )
+                  ],
                 ),
-              )
+              ),
             ],
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
