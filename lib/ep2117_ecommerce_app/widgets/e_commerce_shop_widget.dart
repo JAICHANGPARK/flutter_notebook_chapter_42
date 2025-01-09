010574a24b43283563cb12ba3473ad6a0882cdb2
JAICHANGPARK <aristojeff@gmail.com>
2025-01-09 18:51:20 +0900

    Update e_commerce_shop_widget.dart

--- lib/ep2117_ecommerce_app/widgets/e_commerce_shop_widget.dart
+++ lib/ep2117_ecommerce_app/widgets/e_commerce_shop_widget.dart
@@ -155,7 +155,78 @@ class _ECommerceShopWidgetState extends State<ECommerceShopWidget> {
             child: GridView.builder(
               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
               itemBuilder: (context, index) {
-                
+                return Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200]!,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  padding: EdgeInsets.all(12),
+                  child: Column(
+                    spacing: 16,
+                    children: [
+                      Expanded(
+                        child: Container(
+                          padding: EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(16),
+                            image: DecorationImage(
+                              image: NetworkImage(
+                                "",
+                              ),
+                            ),
+                          ),
+                          child: Align(
+                            alignment: Alignment.topRight,
+                            child: CircleAvatar(
+                              backgroundColor: Colors.grey[200]!,
+                              foregroundColor: Colors.red,
+                              child: Icon(Icons.favorite),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Row(
+                        spacing: 4,
+                        children: [
+                          Text(
+                            "Dream Owens",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                          Spacer(),
+                          Icon(
+                            Icons.star,
+                            color: Colors.orange,
+                            size: 16,
+                          ),
+                          Text("3.5"),
+                        ],
+                      ),
+                      Row(
+                        spacing: 6,
+                        children: [
+                          Text(
+                            "\$2445",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text("\$3445"),
+                          Spacer(),
+                          CircleAvatar(
+                            radius: 18,
+                            backgroundColor: Colors.black,
+                            foregroundColor: Colors.white,
+                            child: Icon(Icons.arrow_forward),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                );
               },
             ),
           )
