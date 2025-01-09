2a111a83b07571eb403872b403858f1017cbf480
JAICHANGPARK <aristojeff@gmail.com>
2025-01-09 18:49:44 +0900

    Update e_commerce_shop_widget.dart

--- lib/ep2117_ecommerce_app/widgets/e_commerce_shop_widget.dart
+++ lib/ep2117_ecommerce_app/widgets/e_commerce_shop_widget.dart
@@ -17,42 +17,57 @@ class _ECommerceShopWidgetState extends State<ECommerceShopWidget> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Row(
+            spacing: 16,
             children: [
+              Expanded(
+                child: Container(
+                  height: 52,
+                  // margin: EdgeInsets.symmetric(horizontal: 16),
+                  padding: EdgeInsets.only(left: 8, right: 4, top: 2, bottom: 2),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.grey[200]!,
+                    ),
+                    borderRadius: BorderRadius.circular(32),
+                  ),
+                  child: Row(
+                    children: [
+                      Expanded(
+                          child: TextField(
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Search",
+                              icon: Icon(Icons.search),
+                            ),
+                          )),
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.grey[200]!,
+                          ),
+                          shape: BoxShape.circle,
+                        ),
+                        child: IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.tune),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
               Container(
-                height: 52,
-                margin: EdgeInsets.symmetric(horizontal: 16),
-                padding: EdgeInsets.only(left: 8, right: 4, top: 2, bottom: 2),
                 decoration: BoxDecoration(
                   border: Border.all(
                     color: Colors.grey[200]!,
                   ),
-                  borderRadius: BorderRadius.circular(32),
+                  shape: BoxShape.circle,
                 ),
-                child: Row(
-                  children: [
-                    Expanded(
-                        child: TextField(
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            hintText: "Search",
-                            icon: Icon(Icons.search),
-                          ),
-                        )),
-                    Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.grey[200]!,
-                        ),
-                        shape: BoxShape.circle,
-                      ),
-                      child: IconButton(
-                        onPressed: () {},
-                        icon: Icon(Icons.tune),
-                      ),
-                    )
-                  ],
+                child: IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.shopping_bag_outlined),
                 ),
-              ),
+              )
             ],
           ),
         ],
