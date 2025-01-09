32a31816273a3c6917dea11c19180a9de6b9bb50
JAICHANGPARK <aristojeff@gmail.com>
2025-01-09 18:39:35 +0900

    Update e_commerce_cart_page.dart

---
+++ lib/ep2117_ecommerce_app/views/e_commerce_cart_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class ECommerceCartPage extends StatefulWidget {
+  const ECommerceCartPage({super.key});
+
+  @override
+  State<ECommerceCartPage> createState() => _ECommerceCartPageState();
+}
+
+class _ECommerceCartPageState extends State<ECommerceCartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
