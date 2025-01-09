eba730c01b0f206a6087eb6956c9fd17902f8ec2
JAICHANGPARK <aristojeff@gmail.com>
2025-01-09 18:46:02 +0900

    add

---
+++ lib/ep2117_ecommerce_app/widgets/e_commerce_shop_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class ECommerceShopWidget extends StatefulWidget {
+  const ECommerceShopWidget({super.key});
+
+  @override
+  State<ECommerceShopWidget> createState() => _ECommerceShopWidgetState();
+}
+
+class _ECommerceShopWidgetState extends State<ECommerceShopWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
