f471b85d2ec55b7396e308939058a41ac6a29e96
JAICHANGPARK <aristojeff@gmail.com>
2025-01-09 18:47:33 +0900

    Update e_commerce_shop_widget.dart

--- lib/ep2117_ecommerce_app/widgets/e_commerce_shop_widget.dart
+++ lib/ep2117_ecommerce_app/widgets/e_commerce_shop_widget.dart
@@ -11,7 +11,46 @@ class ECommerceShopWidget extends StatefulWidget {
 class _ECommerceShopWidgetState extends State<ECommerceShopWidget> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          height: 52,
+          margin: EdgeInsets.symmetric(horizontal: 16),
+          padding: EdgeInsets.only(left: 8, right: 4, top: 2, bottom: 2),
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Colors.grey[200]!,
+            ),
+            borderRadius: BorderRadius.circular(32),
+          ),
+          child: Row(
+            children: [
+              Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Search",
+                      icon: Icon(Icons.search),
+                    ),
+                  )),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey[200]!,
+                  ),
+                  shape: BoxShape.circle,
+                ),
+                child: IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.tune),
+                ),
+              )
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
 
