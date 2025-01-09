857acbe1b38594dbcd969e66b1042362f01bb900
JAICHANGPARK <aristojeff@gmail.com>
2025-01-09 18:41:42 +0900

    Update e_commerce_shop_page.dart

---
+++ lib/ep2117_ecommerce_app/views/e_commerce_shop_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class ECommerceShopPage extends StatefulWidget {
+  const ECommerceShopPage({super.key});
+
+  @override
+  State<ECommerceShopPage> createState() => _ECommerceShopPageState();
+}
+
+class _ECommerceShopPageState extends State<ECommerceShopPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
