2a09dc1bb87e0e6408cc695f8eb664892c9d25d2
JAICHANGPARK <aristojeff@gmail.com>
2025-01-12 09:07:24 +0900

    Update ep2124_main.dart

--- lib/ep2124_hr_app/ep2124_main.dart
+++ lib/ep2124_hr_app/ep2124_main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  
+  runApp(HrApp());
 }
 
 class HrApp extends StatelessWidget {
@@ -9,6 +9,6 @@ class HrApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp();
   }
 }
