511553ad0f59163618e8e39d0b13390fc5a6824d
JAICHANGPARK <aristojeff@gmail.com>
2025-01-12 09:12:47 +0900

    Update hr_select_screen.dart

--- lib/ep2124_hr_app/views/hr_select_screen.dart
+++ lib/ep2124_hr_app/views/hr_select_screen.dart
@@ -13,29 +13,33 @@ class _HrSelectScreenState extends State<HrSelectScreen> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            spacing: 16,
-            children: [
-              Text(
-                "Select Your Role",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              spacing: 16,
+              children: [
+                Text(
+                  "Select Your Role",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
                 ),
-              ),
-              Text(
-                "Select the role which suits your need",
-              ),
-              Container(
-                height: 240,
-                decoration: BoxDecoration(
-                  color: Color(0xff33bf7f),
+                Text(
+                  "Select the role which suits your need",
                 ),
-              )
-            ],
+                Container(
+                  height: 240,
+
+                  decoration: BoxDecoration(
+                    color: Color(0xff33bf7f),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
