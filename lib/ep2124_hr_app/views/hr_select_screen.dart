cd51b69a1b8a765838b34ae2521e899cde5c4321
JAICHANGPARK <aristojeff@gmail.com>
2025-01-12 09:08:59 +0900

    Update hr_select_screen.dart

---
+++ lib/ep2124_hr_app/views/hr_select_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class HrSelectScreen extends StatefulWidget {
+  const HrSelectScreen({super.key});
+
+  @override
+  State<HrSelectScreen> createState() => _HrSelectScreenState();
+}
+
+class _HrSelectScreenState extends State<HrSelectScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
