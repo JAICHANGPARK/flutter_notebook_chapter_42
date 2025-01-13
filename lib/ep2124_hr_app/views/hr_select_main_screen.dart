58c384772615b6b7f3423975eaed79772d5ceeb7
JAICHANGPARK <aristojeff@gmail.com>
2025-01-13 18:25:10 +0900

    Update hr_select_main_screen.dart

--- lib/ep2124_hr_app/views/hr_select_main_screen.dart
+++ lib/ep2124_hr_app/views/hr_select_main_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:hugeicons/hugeicons.dart';
 
 class HrSelectMainScreen extends StatefulWidget {
   const HrSelectMainScreen({super.key});
@@ -13,8 +13,14 @@ class _HrSelectMainScreenState extends State<HrSelectMainScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: [
-
+        children: [],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(HugeIcons.strokeRoundedHome01),
+            label: "Home",
+          ),
         ],
       ),
     );
