f2b417a223f8c2d55e1eb92c7001dfc5dbc589f5
JAICHANGPARK <aristojeff@gmail.com>
2025-01-13 18:27:08 +0900

    Update hr_select_main_screen.dart

--- lib/ep2124_hr_app/views/hr_select_main_screen.dart
+++ lib/ep2124_hr_app/views/hr_select_main_screen.dart
@@ -16,28 +16,31 @@ class _HrSelectMainScreenState extends State<HrSelectMainScreen> {
       body: Column(
         children: [],
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Color(0xFF33BF7F),
-        backgroundColor: Colors.white,
-        type: BottomNavigationBarType.fixed,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(HugeIcons.strokeRoundedHome01),
-            label: "Home",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(HugeIcons.strokeRoundedBookmark01),
-            label: "Save",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(HugeIcons.strokeRoundedCheckList),
-            label: "Summary",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(HugeIcons.strokeRoundedProfile),
-            label: "Profile",
-          ),
-        ],
+      bottomNavigationBar: SizedBox(
+        height: 80,
+        child: BottomNavigationBar(
+          selectedItemColor: Color(0xFF33BF7F),
+          backgroundColor: Colors.white,
+          type: BottomNavigationBarType.fixed,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(HugeIcons.strokeRoundedHome01),
+              label: "Home",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(HugeIcons.strokeRoundedBookmark01),
+              label: "Save",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(HugeIcons.strokeRoundedCheckList),
+              label: "Summary",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(HugeIcons.strokeRoundedProfile),
+              label: "Profile",
+            ),
+          ],
+        ),
       ),
     );
   }
