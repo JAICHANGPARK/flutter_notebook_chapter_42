d685b07e639c0c22c4267f0b015a337361279697
JAICHANGPARK <aristojeff@gmail.com>
2025-01-13 18:22:18 +0900

    Update hr_select_main_screen.dart

---
+++ lib/ep2124_hr_app/views/hr_select_main_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class HrSelectMainScreen extends StatefulWidget {
+  const HrSelectMainScreen({super.key});
+
+  @override
+  State<HrSelectMainScreen> createState() => _HrSelectMainScreenState();
+}
+
+class _HrSelectMainScreenState extends State<HrSelectMainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
