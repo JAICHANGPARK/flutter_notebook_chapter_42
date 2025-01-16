48fdd754deb89f2eeb9cacdd7ce3c95f4c6cb872
JAICHANGPARK <aristojeff@gmail.com>
2025-01-16 18:33:18 +0900

    Update hr_otp_page.dart

---
+++ lib/ep2124_hr_app/views/hr_otp_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class HrOtpPage extends StatefulWidget {
+  const HrOtpPage({super.key});
+
+  @override
+  State<HrOtpPage> createState() => _HrOtpPageState();
+}
+
+class _HrOtpPageState extends State<HrOtpPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
