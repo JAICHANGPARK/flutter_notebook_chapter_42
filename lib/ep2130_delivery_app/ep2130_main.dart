71306c107c653769166634a08bd63dd91da2b06f
JAICHANGPARK <aristojeff@gmail.com>
2025-01-18 08:39:09 +0900

    Update ep2130_main.dart

--- lib/ep2130_delivery_app/ep2130_main.dart
+++ lib/ep2130_delivery_app/ep2130_main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(DeliveryApp());
+}
 
 class DeliveryApp extends StatelessWidget {
   const DeliveryApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp();
   }
 }
