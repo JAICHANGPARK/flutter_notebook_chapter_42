0f173118e15c2d1fd9210192de998bac2fd392b4
JAICHANGPARK <aristojeff@gmail.com>
2025-01-24 05:50:35 +0900

    Create ep2136_main.dart

---
+++ lib/ep2136_sales_filters_sort/ep2136_main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+
+class SalesApp extends StatelessWidget {
+  const SalesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
