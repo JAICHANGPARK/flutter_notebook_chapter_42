77513c99f64dc769aa6e9b759792a7e220cd63c8
JAICHANGPARK <aristojeff@gmail.com>
2025-01-24 05:51:24 +0900

    Create sales_main_page.dart

---
+++ lib/ep2136_sales_filters_sort/views/sales_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class SalesMainPage extends StatefulWidget {
+  const SalesMainPage({super.key});
+
+  @override
+  State<SalesMainPage> createState() => _SalesMainPageState();
+}
+
+class _SalesMainPageState extends State<SalesMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
