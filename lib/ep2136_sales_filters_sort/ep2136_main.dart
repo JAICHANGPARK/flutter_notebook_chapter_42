3924367c131257ea3afad06147c1873d13959f6c
JAICHANGPARK <aristojeff@gmail.com>
2025-01-24 05:53:46 +0900

    Update ep2136_main.dart

--- lib/ep2136_sales_filters_sort/ep2136_main.dart
+++ lib/ep2136_sales_filters_sort/ep2136_main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_42/ep2136_sales_filters_sort/views/sales_main_page.dart';
 
 void main() {
   runApp(SalesApp());
@@ -9,6 +10,8 @@ class SalesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: SalesMainPage(),
+    );
   }
 }
