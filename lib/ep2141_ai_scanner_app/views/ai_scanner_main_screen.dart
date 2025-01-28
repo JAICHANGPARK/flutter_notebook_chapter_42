390e6823e367ed3791efba41b7811fa23dda02c5
JAICHANGPARK <aristojeff@gmail.com>
2025-01-29 08:21:50 +0900

    Create ai_scanner_main_screen.dart

---
+++ lib/ep2141_ai_scanner_app/views/ai_scanner_main_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class AiScannerMainScreen extends StatefulWidget {
+  const AiScannerMainScreen({super.key});
+
+  @override
+  State<AiScannerMainScreen> createState() => _AiScannerMainScreenState();
+}
+
+class _AiScannerMainScreenState extends State<AiScannerMainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
