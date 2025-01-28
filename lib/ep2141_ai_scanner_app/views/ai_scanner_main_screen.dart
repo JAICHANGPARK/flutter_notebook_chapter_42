e98c39be772017118b03137a1f8056d889fb2fb3
JAICHANGPARK <aristojeff@gmail.com>
2025-01-29 08:33:59 +0900

    Update ai_scanner_main_screen.dart

--- lib/ep2141_ai_scanner_app/views/ai_scanner_main_screen.dart
+++ lib/ep2141_ai_scanner_app/views/ai_scanner_main_screen.dart
@@ -68,6 +68,10 @@ class _AiScannerMainScreenState extends State<AiScannerMainScreen> {
                   ],
                 ),
               ),
+              Container(
+                height: 80,
+                child: Placeholder(),
+              )
             ],
           ),
         ),
