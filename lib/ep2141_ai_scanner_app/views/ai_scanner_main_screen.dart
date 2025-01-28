179bba650e4f720130540f9c7cd6394efe2e135f
JAICHANGPARK <aristojeff@gmail.com>
2025-01-29 08:30:57 +0900

    Update ai_scanner_main_screen.dart

--- lib/ep2141_ai_scanner_app/views/ai_scanner_main_screen.dart
+++ lib/ep2141_ai_scanner_app/views/ai_scanner_main_screen.dart
@@ -13,28 +13,31 @@ class _AiScannerMainScreenState extends State<AiScannerMainScreen> {
     return Scaffold(
       backgroundColor: Color.fromRGBO(242, 242, 242, 1),
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.info_outline),
-                ),
-                Badge(
-                  child: Icon(
-                    Icons.notifications_none,
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.info_outline),
                   ),
-                  alignment: Alignment.topLeft,
-                  label: Text("5"),
-                ),
-              ],
-            ),
-            Text("Hi Dream,"),
-            Text("How can I help\nyou today?"),
-          ],
+                  Badge(
+                    child: Icon(
+                      Icons.notifications_none,
+                    ),
+                    alignment: Alignment.topLeft,
+                    label: Text("5"),
+                  ),
+                ],
+              ),
+              Text("Hi Dream,"),
+              Text("How can I help\nyou today?"),
+            ],
+          ),
         ),
       ),
     );
