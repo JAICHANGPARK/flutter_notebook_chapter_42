679a46d1b7da53cebe2b2587be69b3ba14b5c280
JAICHANGPARK <aristojeff@gmail.com>
2025-01-30 10:35:45 +0900

    Update ai_scanner_main_screen.dart

--- lib/ep2141_ai_scanner_app/views/ai_scanner_main_screen.dart
+++ lib/ep2141_ai_scanner_app/views/ai_scanner_main_screen.dart
@@ -39,198 +39,212 @@ class _AiScannerMainScreenState extends State<AiScannerMainScreen> {
               Container(
                 height: 110,
               ),
-              Expanded(child: SingleChildScrollView(child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                spacing: 16,
-                children: [
-
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Hi Dream,",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Color.fromRGBO(115, 120, 119, 1),
-                          fontSize: 42,
-                          height: 1.2,
-                        ),
-                      ),
-                      Text(
-                        "How can I help\nyou today?",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 42,
-                          height: 1.2,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Gap(24),
-                  Container(
-                    height: 280,
-                    child: Row(
-                      spacing: 8,
+              Expanded(
+                  child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  spacing: 16,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Expanded(
-                            child: Column(
-                              spacing: 8,
-                              children: [
-                                Expanded(
-                                    child: Container(
-                                      decoration: BoxDecoration(
-                                        color: Colors.white,
-                                        borderRadius: BorderRadius.circular(16),
-                                      ),
-                                      width: double.infinity,
-                                      padding: EdgeInsets.all(16),
-                                      child: Column(
-                                        crossAxisAlignment: CrossAxisAlignment.start,
-                                        mainAxisAlignment: MainAxisAlignment.center,
-                                        children: [
-                                          Icon(Icons.document_scanner_outlined),
-                                          Gap(12),
-                                          Text(
-                                            "Scan",
-                                            style: TextStyle(
-                                              fontSize: 18,
-                                              fontWeight: FontWeight.bold,
-                                            ),
-                                          ),
-                                          Text(
-                                            "Documents, ID cards...",
-                                          )
-                                        ],
-                                      ),
-                                    )),
-                                Expanded(
-                                    child: Container(
-                                      decoration: BoxDecoration(
-                                        color: Colors.white,
-                                        borderRadius: BorderRadius.circular(16),
-                                      ),
-                                      width: double.infinity,
-                                      padding: EdgeInsets.all(16),
-                                      child: Column(
-                                        crossAxisAlignment: CrossAxisAlignment.start,
-                                        mainAxisAlignment: MainAxisAlignment.center,
-                                        children: [
-                                          Icon(Icons.document_scanner_outlined),
-                                          Gap(12),
-                                          Text(
-                                            "Edit",
-                                            style: TextStyle(
-                                              fontSize: 18,
-                                              fontWeight: FontWeight.bold,
-                                            ),
-                                          ),
-                                          Text(
-                                            "Documents, ID cards...",
-                                          )
-                                        ],
-                                      ),
-                                    )),
-                              ],
-                            )),
-                        Expanded(
-                            child: Column(
-                              spacing: 8,
-                              children: [
-                                Expanded(
-                                    child: Container(
-                                      decoration: BoxDecoration(
-                                        color: Colors.white,
-                                        borderRadius: BorderRadius.circular(16),
-                                      ),
-                                      width: double.infinity,
-                                      padding: EdgeInsets.all(16),
-                                      child: Column(
-                                        crossAxisAlignment: CrossAxisAlignment.start,
-                                        mainAxisAlignment: MainAxisAlignment.center,
-                                        children: [
-                                          Icon(Icons.document_scanner_outlined),
-                                          Gap(12),
-                                          Text(
-                                            "Convert",
-                                            style: TextStyle(
-                                              fontSize: 18,
-                                              fontWeight: FontWeight.bold,
-                                            ),
-                                          ),
-                                          Text(
-                                            "Documents, ID cards...",
-                                          )
-                                        ],
-                                      ),
-                                    )),
-                                Expanded(
-                                    child: Container(
-                                      decoration: BoxDecoration(
-                                        color: Colors.white,
-                                        borderRadius: BorderRadius.circular(16),
-                                      ),
-                                      width: double.infinity,
-                                      padding: EdgeInsets.all(16),
-                                      child: Column(
-                                        crossAxisAlignment: CrossAxisAlignment.start,
-                                        mainAxisAlignment: MainAxisAlignment.center,
-                                        children: [
-                                          Icon(Icons.document_scanner_outlined),
-                                          Gap(12),
-                                          Text(
-                                            "Ask AI",
-                                            style: TextStyle(
-                                              fontSize: 18,
-                                              fontWeight: FontWeight.bold,
-                                            ),
-                                          ),
-                                          Text(
-                                            "Documents, ID cards...",
-                                          )
-                                        ],
-                                      ),
-                                    )),
-                              ],
-                            )),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      // color: Colors.blue,
-                      border: Border.all(
-                        color: Colors.grey,
-                      ),
-                      borderRadius: BorderRadius.circular(24),
-                    ),
-                    child: Row(
-                      children: [
-                        IconButton(
-                          onPressed: () {},
-                          icon: Icon(Icons.search),
-                        ),
-                        Expanded(
-                          child: TextField(
-                            decoration: InputDecoration(
-                              hintText: "Search",
-                              border: InputBorder.none,
-                            ),
+                        Text(
+                          "Hi Dream,",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromRGBO(115, 120, 119, 1),
+                            fontSize: 42,
+                            height: 1.2,
                           ),
                         ),
-                        IconButton(
-                          onPressed: () {},
-                          icon: Icon(Icons.mic),
+                        Text(
+                          "How can I help\nyou today?",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 42,
+                            height: 1.2,
+                          ),
                         ),
                       ],
                     ),
-                  ),
-                  Container(
-                    height: 80,
-                    child: Placeholder(),
-                  )
-                ],
-              ),))
-
+                    Gap(24),
+                    Container(
+                      height: 280,
+                      child: Row(
+                        spacing: 8,
+                        children: [
+                          Expanded(
+                              child: Column(
+                            spacing: 8,
+                            children: [
+                              Expanded(
+                                  child: Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(16),
+                                ),
+                                width: double.infinity,
+                                padding: EdgeInsets.all(16),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Icon(Icons.document_scanner_outlined),
+                                    Gap(12),
+                                    Text(
+                                      "Scan",
+                                      style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    Text(
+                                      "Documents, ID cards...",
+                                    )
+                                  ],
+                                ),
+                              )),
+                              Expanded(
+                                  child: Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(16),
+                                ),
+                                width: double.infinity,
+                                padding: EdgeInsets.all(16),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Icon(Icons.document_scanner_outlined),
+                                    Gap(12),
+                                    Text(
+                                      "Edit",
+                                      style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    Text(
+                                      "Documents, ID cards...",
+                                    )
+                                  ],
+                                ),
+                              )),
+                            ],
+                          )),
+                          Expanded(
+                              child: Column(
+                            spacing: 8,
+                            children: [
+                              Expanded(
+                                  child: Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(16),
+                                ),
+                                width: double.infinity,
+                                padding: EdgeInsets.all(16),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Icon(Icons.document_scanner_outlined),
+                                    Gap(12),
+                                    Text(
+                                      "Convert",
+                                      style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    Text(
+                                      "Documents, ID cards...",
+                                    )
+                                  ],
+                                ),
+                              )),
+                              Expanded(
+                                  child: Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(16),
+                                ),
+                                width: double.infinity,
+                                padding: EdgeInsets.all(16),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Icon(Icons.document_scanner_outlined),
+                                    Gap(12),
+                                    Text(
+                                      "Ask AI",
+                                      style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    Text(
+                                      "Documents, ID cards...",
+                                    )
+                                  ],
+                                ),
+                              )),
+                            ],
+                          )),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        // color: Colors.blue,
+                        border: Border.all(
+                          color: Colors.grey,
+                        ),
+                        borderRadius: BorderRadius.circular(24),
+                      ),
+                      child: Row(
+                        children: [
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.search),
+                          ),
+                          Expanded(
+                            child: TextField(
+                              decoration: InputDecoration(
+                                hintText: "Search",
+                                border: InputBorder.none,
+                              ),
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.mic),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      height: 80,
+                      child: Row(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(color: Colors.black),
+                          ),
+                          CircleAvatar(
+                            backgroundColor: Colors.black,
+                            foregroundColor: Colors.white,
+                            child: Icon(
+                              Icons.add_circle_outline,
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ))
             ],
           ),
         ),
