20a6c6162a3dfb8d77c2bd4254a2ed3f817cd5a8
JAICHANGPARK <aristojeff@gmail.com>
2025-01-31 18:37:06 +0900

    Update food_order_home_page.dart

--- lib/ep2143_food_order_app/views/food_order_home_page.dart
+++ lib/ep2143_food_order_app/views/food_order_home_page.dart
@@ -13,33 +13,39 @@ class _FoodOrderHomePageState extends State<FoodOrderHomePage> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Container(
-                  child: IconButton(onPressed: (){}, icon: Icon(Icons.menu)),
-                ),
-                Expanded(child: Center(
-                  child: Container(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      spacing: 8,
-                      children: [
-                        Container(
-                          child: Text("Dine In"),
-                        ),
-                        Container(
-                          child: Text("Take Away"),
-                        )
-
-                      ],
-                    ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Container(
+                    child: IconButton(onPressed: (){}, icon: Icon(Icons.menu)),
                   ),
-                ))
-              ],
-            )
-          ],
+                  Expanded(child: Center(
+                    child: Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        spacing: 8,
+                        children: [
+                          Container(
+                            child: Text("Dine In"),
+                          ),
+                          Container(
+                            child: Text("Take Away"),
+                          )
+
+                        ],
+                      ),
+                    ),
+                  )),
+                  Container(
+                    child: Badge(child: Icon(Icons.notifications_active_outlined),),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
