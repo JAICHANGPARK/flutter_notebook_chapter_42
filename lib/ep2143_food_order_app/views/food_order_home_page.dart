ec289a8fb6f35765d107841963f405967d3e014f
JAICHANGPARK <aristojeff@gmail.com>
2025-02-03 17:32:52 +0900

    Update food_order_home_page.dart

--- lib/ep2143_food_order_app/views/food_order_home_page.dart
+++ lib/ep2143_food_order_app/views/food_order_home_page.dart
@@ -87,18 +87,25 @@ class _FoodOrderHomePageState extends State<FoodOrderHomePage> {
                       showModalBottomSheet(
                         context: context,
                         isScrollControlled: true,
-
                         builder: (context) {
                           return Container(
                             height: MediaQuery.sizeOf(context).height - 180,
                             decoration: BoxDecoration(
                               color: Colors.white,
                               borderRadius: BorderRadius.circular(24),
-
                             ),
                             child: Stack(
                               children: [
-
+                                Positioned(
+                                  bottom: 24,
+                                    left: 16,
+                                    right: 16,
+                                    child: Container(
+                                  decoration: ShapeDecoration(
+                                    shape: StadiumBorder(),
+                                  ),
+                                      
+                                ))
                               ],
                             ),
                           );
