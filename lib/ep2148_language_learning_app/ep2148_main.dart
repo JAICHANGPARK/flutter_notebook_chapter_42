bfa8292cb06cf55d2f15ddde72d60691ed021253
JAICHANGPARK <aristojeff@gmail.com>
2025-02-05 18:18:37 +0900

    Update ep2148_main.dart

--- lib/ep2148_language_learning_app/ep2148_main.dart
+++ lib/ep2148_language_learning_app/ep2148_main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'views/lla_home_page.dart';
+
 void main() {
   runApp(LanguageLearningApp());
 
@@ -11,7 +13,7 @@ class LanguageLearningApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ,
+      home: LanguageLearningHomePage(),
     );
   }
 }
