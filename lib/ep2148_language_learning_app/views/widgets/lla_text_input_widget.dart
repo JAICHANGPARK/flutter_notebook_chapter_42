5b5023eced07b8d941fe67ce73bb3504e5354412
JAICHANGPARK <aristojeff@gmail.com>
2025-02-07 17:24:24 +0900

    Create lla_text_input_widget.dart

---
+++ lib/ep2148_language_learning_app/views/widgets/lla_text_input_widget.dart
@@ -0,0 +1,13 @@
+class LlaTextInputWidget extends StatefulWidget {
+  const LlaTextInputWidget({super.key});
+
+  @override
+  State<LlaTextInputWidget> createState() => _LlaTextInputWidgetState();
+}
+
+class _LlaTextInputWidgetState extends State<LlaTextInputWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
