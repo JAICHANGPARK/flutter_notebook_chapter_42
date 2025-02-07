5e2586915e1793b0ed48c5c769996da1ab3eeb8b
JAICHANGPARK <aristojeff@gmail.com>
2025-02-07 17:23:07 +0900

    Update favorite_topic_widget.dart

---
+++ lib/ep2148_language_learning_app/views/widgets/favorite_topic_widget.dart
@@ -0,0 +1,13 @@
+class FavoriteTopicWidget extends StatefulWidget {
+  const FavoriteTopicWidget({super.key});
+
+  @override
+  State<FavoriteTopicWidget> createState() => _FavoriteTopicWidgetState();
+}
+
+class _FavoriteTopicWidgetState extends State<FavoriteTopicWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
