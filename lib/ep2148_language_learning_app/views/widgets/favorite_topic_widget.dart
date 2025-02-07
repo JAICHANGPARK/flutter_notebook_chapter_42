b8853e1cb6385ebeb02e7773a9177eed1d43bbc5
JAICHANGPARK <aristojeff@gmail.com>
2025-02-07 17:23:14 +0900

    Update favorite_topic_widget.dart

--- lib/ep2148_language_learning_app/views/widgets/favorite_topic_widget.dart
+++ lib/ep2148_language_learning_app/views/widgets/favorite_topic_widget.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 class FavoriteTopicWidget extends StatefulWidget {
   const FavoriteTopicWidget({super.key});
 
@@ -8,6 +10,81 @@ class FavoriteTopicWidget extends StatefulWidget {
 class _FavoriteTopicWidgetState extends State<FavoriteTopicWidget> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return   Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(4),
+      ),
+      child: Column(
+        spacing: 8,
+        children: [
+          ListTile(
+            title: Text(
+              "Famous Speeches",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+            subtitle: Text("I have a dream"),
+            leading: Icon(Icons.campaign_outlined),
+            trailing: Icon(
+              Icons.chevron_right,
+            ),
+          ),
+          Divider(
+            height: 8,
+          ),
+          ListTile(
+            title: Text(
+              "Famous Speeches",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+            subtitle: Text("I have a dream"),
+            leading: Icon(Icons.campaign_outlined),
+            trailing: Icon(
+              Icons.chevron_right,
+            ),
+          ),
+          Divider(
+            height: 8,
+          ),
+          ListTile(
+            title: Text(
+              "Famous Speeches",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+            subtitle: Text("I have a dream"),
+            leading: Icon(Icons.campaign_outlined),
+            trailing: Icon(
+              Icons.chevron_right,
+            ),
+          ),
+          Divider(
+            height: 8,
+          ),
+          ListTile(
+            title: Text(
+              "Famous Speeches",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+            subtitle: Text("I have a dream"),
+            leading: Icon(Icons.campaign_outlined),
+            trailing: Icon(
+              Icons.chevron_right,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
