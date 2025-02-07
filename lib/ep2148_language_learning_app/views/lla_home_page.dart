7660773d0dfbb19058e1b246b5785f6badf48946
JAICHANGPARK <aristojeff@gmail.com>
2025-02-07 17:16:04 +0900

    Update lla_home_page.dart

--- lib/ep2148_language_learning_app/views/lla_home_page.dart
+++ lib/ep2148_language_learning_app/views/lla_home_page.dart
@@ -105,34 +105,7 @@ class _LanguageLearningHomePageState extends State<LanguageLearningHomePage> {
                       )
                     ],
                   ),
-                  // SizedBox(
-                  //   height: 80,
-                  //   width: double.infinity,
-                  //   child: Stepper(
-                  //     elevation: 0,
-                  //     currentStep: 0,
-                  //     physics: BouncingScrollPhysics(),
-                  //     controller: ScrollController(),
-                  //     steps: [
-                  //       Step(
-                  //           title: Text("milestone"),
-                  //           content: Icon(
-                  //             Icons.check,
-                  //           )),
-                  //       Step(
-                  //           title: Text("milestone"),
-                  //           content: Icon(
-                  //             Icons.check,
-                  //           )),
-                  //       Step(
-                  //           title: Text("milestone"),
-                  //           content: Icon(
-                  //             Icons.check,
-                  //           ))
-                  //     ],
-                  //     type: StepperType.horizontal,
-                  //   ),
-                  // ),
+              
                   Gap(12),
                   Container(
                     height: 64,
