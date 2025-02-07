56b8ea1870ecfb1c6e3699c0b637092c2af99efd
JAICHANGPARK <aristojeff@gmail.com>
2025-02-07 17:15:45 +0900

    Create stepper_widget.dart

---
+++ lib/ep2148_language_learning_app/views/widgets/stepper_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class LearningStepperWidget extends StatefulWidget {
+  const LearningStepperWidget({super.key});
+
+  @override
+  State<LearningStepperWidget> createState() => _LearningStepperWidgetState();
+}
+
+class _LearningStepperWidgetState extends State<LearningStepperWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
